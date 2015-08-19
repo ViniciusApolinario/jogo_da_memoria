945b7ab7c24eeb05897721fa2cecc6a0a164cc45
Nicolas Bezerra <nicolasbezerra14@gmail.com>
2015-08-19 12:21:16 -0300

    Fiz o esqueleto
    Foram feitas as cartas

---
+++ Assets/scripts/Cards.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cards : MonoBehaviour {
+
+	public GameObject cartas, canvas;
+	public Transform posiçao;
+	private GameObject linha;
+	public 
+	// Use this for initialization
+	void Start () {
+
+		for (int ue = 0; ue < 6; ue++) {
+			GameObject linha = Instantiate (cartas) as GameObject;
+			posiçao.transform.position = new Vector3(posiçao.transform.position.x + ue + 100 , posiçao.transform.position.y , transform.position.z); 
+			linha.transform.SetParent (canvas.transform);
+			linha.transform.localPosition = posiçao.transform.position;
+			linha.transform.rotation = Quaternion.identity;
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/scripts/Carta.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Carta : MonoBehaviour {
+	public GameObject[] imagens;
+	// Use this for initializtion
+	void Start () {
+		for (int tw = 1; tw < 9; tw++) {
+			imagens[] = Resources.Load("carros1") as GameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
