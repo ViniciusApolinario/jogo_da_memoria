3b406423b6d1e0375be6732993a0376a44fc4f46
Nicolas Bezerra <nicolasbezerra14@gmail.com>
2015-08-25 09:29:08 -0300

    nada
    nada

---
+++ Assets/scripts/Ativa.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ativa : MonoBehaviour {
+
+	public bool ativada = false;
+
+	public void lol(){
+		ativada = true;
+		if(ativada){
+			ativada = false;
+		}
+	}
+}
--- Assets/scripts/Carta.cs
+++ Assets/scripts/Carta.cs
@@ -10,13 +10,14 @@ public class Carta : MonoBehaviour {
 
 	// Use this for initializtion
 	void Start () {
+
 		imagens = Resources.LoadAll<Sprite> (way);
 		batata = GetComponent<Button> ();
 		//batata.image = imagens [Random.Range (0, imagens.Length)];
 		//batata.sprite = imagens [Random.Range (0, imagens.Length)];
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
