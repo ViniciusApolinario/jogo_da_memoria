32ba579b3a309d5e5bed6c9581fa523a490580bf
ViniciusApolinario <viniapolinario.nave@gmail.com>
2015-08-25 23:56:28 -0300

    Mais ajustes
    -esta formando pares , só tem que ajsutar as imagens

--- Assets/scripts/Cards.cs
+++ Assets/scripts/Cards.cs
@@ -6,20 +6,37 @@ public class Cards : MonoBehaviour {
 	public GameObject cartas, canvas;
 	public Transform posiçao;
 	public static GameObject[] linha;
+	public static GameObject[] par;
+	public static int[] teams;
+
+	private int numbercards = 0;
+	private int t;
 	//public static int ue;
 	// Use this for initialization
 	void Start () {
 		linha = new GameObject[6];
-		for (int ue = 0; ue < 6; ue++) {
-			linha[ue] = Instantiate (cartas) as GameObject;
-			posiçao.transform.position = new Vector3(posiçao.transform.position.x + ue + 100 , posiçao.transform.position.y , transform.position.z); 
-			linha[ue].transform.SetParent (canvas.transform);
-			linha[ue].transform.localPosition = posiçao.transform.position;
-			linha[ue].transform.rotation = Quaternion.identity;
-			linha[ue].name = ue.ToString();
 
+		for (numbercards = 0; numbercards < 6; numbercards++) {
+			linha[numbercards] = Instantiate (cartas) as GameObject;
+			posiçao.transform.position = new Vector3(posiçao.transform.position.x + numbercards + 100 , posiçao.transform.position.y , transform.position.z); 
+			linha[numbercards].transform.SetParent (canvas.transform);
+			linha[numbercards].transform.localPosition = posiçao.transform.position;
+			linha[numbercards].transform.rotation = Quaternion.identity;
+			linha[numbercards].name = numbercards.ToString();
 		}
 
+		teams = new int[3];
+		teams[0] = Random.Range (0, 6);
+		teams[1] = Random.Range (0, 6);
+		teams[2] = Random.Range (0, 6);
+		if (teams[0] == teams[1] || teams[1] == teams[2] || teams[0] == teams[2]) {
+			teams[0] = Random.Range (0, 6);
+			teams[1] = Random.Range (0, 6);
+			teams[2] = Random.Range (0, 6);
+		}
+		Debug.Log (teams[0]);
+		Debug.Log (teams[1]);
+		Debug.Log (teams[2]);
 
 	}
 
--- Assets/scripts/Carta.cs
+++ Assets/scripts/Carta.cs
@@ -15,20 +15,21 @@ public class Carta : MonoBehaviour {
 	void Start () {
 		//batata.image = imagens [Random.Range (0, imagens.Length)];
 		IDC = gameObject.name;
-		Debug.Log (IDC);
 		imagens = Resources.LoadAll<Sprite> (way);
 		batata = GetComponent<Image> ();
 		foto = imagens[Random.Range (0, imagens.Length)];
-
+		if (Cards.teams[0].ToString() == IDC || Cards.teams [1].ToString() == IDC ||Cards.teams [2].ToString() == IDC ) {
+			Debug.Log("Essa e a parte que vc organiza os spritees");
+		}
 
 
 	}
 	public void lol(){
 		batata.sprite = foto;
-		Debug.Log ("clickou");
+
 	}
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
